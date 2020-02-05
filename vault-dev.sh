a6f4c79055f620bde6bc57828c5e5c01a323f214
seru1us <seancmccully@gmail.com>
2020-02-04 22:08:27 -0500

    testing bash shortcuts for commiting

--- vault-dev.sh
+++ vault-dev.sh
@@ -4,7 +4,7 @@ rm -rf logfile.log
 
 ./vault server -dev -dev-listen-address=127.0.0.1:7200 -dev-root-token-id="root" -log-level="info" -dev-plugin-dir="plugins" &>> logfile.log &
 
-PATH=$(getconf PATH)
+source /etc/environment
 export VAULT_ADDR=http://127.0.0.1:7200
 export VAULT_DEV_ROOT_TOKEN_ID="root"
 export PATH=$PATH:.
@@ -12,7 +12,6 @@ sleep 1
 
 printf "\n"
 grep "Unseal Key:" logfile.log
-
 printf "\n"
 printf "Current Vault Server PID: "
 pgrep -f "./vault server"
