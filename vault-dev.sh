bd6e647d3ca12f35dff71bf38d9a9a1a9295c46b
seru1us <seancmccully@gmail.com>
2020-02-04 22:38:32 -0500

    added in support for plugin directories and stuff

--- vault-dev.sh
+++ vault-dev.sh
@@ -1,6 +1,7 @@
 #!/usr/bin/bash
 
 rm -rf logfile.log
+mkdir -p plugins
 
 ./vault server -dev -dev-listen-address=127.0.0.1:7200 -dev-root-token-id="root" -log-level="info" -dev-plugin-dir="plugins" &>> logfile.log &
 
@@ -8,10 +9,14 @@ source /etc/environment
 export VAULT_ADDR=http://127.0.0.1:7200
 export VAULT_DEV_ROOT_TOKEN_ID="root"
 export PATH=$PATH:.
-sleep 1
+sleep 2
 
 printf "\n"
 grep "Unseal Key:" logfile.log
 printf "\n"
+printf "VAULT_DEV_ROOT_TOKEN_ID: root"
+printf "\n"
 printf "Current Vault Server PID: "
 pgrep -f "./vault server"
+
+
