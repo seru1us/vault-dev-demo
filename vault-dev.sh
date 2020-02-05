5a18978b00bde1b771fb4db304de41180f82b1d6
seru1us <seancmccully@gmail.com>
2020-02-04 22:01:36 -0500

    got this working with the expected output

---
+++ vault-dev.sh
@@ -0,0 +1,18 @@
+#!/usr/bin/bash
+
+rm -rf logfile.log
+
+./vault server -dev -dev-listen-address=127.0.0.1:7200 -dev-root-token-id="root" -log-level="info" -dev-plugin-dir="plugins" &>> logfile.log &
+
+PATH=$(getconf PATH)
+export VAULT_ADDR=http://127.0.0.1:7200
+export VAULT_DEV_ROOT_TOKEN_ID="root"
+export PATH=$PATH:.
+sleep 1
+
+printf "\n"
+grep "Unseal Key:" logfile.log
+
+printf "\n"
+printf "Current Vault Server PID: "
+pgrep -f "./vault server"
